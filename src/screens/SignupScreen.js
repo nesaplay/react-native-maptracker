7c039a150315ceafadb2c00ebad1ab8f3dc1bcc1
Nenad Radovanovic <nash.radovanovich@gmail.com>
2019-09-22 18:22:20 +0200

    added elements, spacer and light work on signup

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-      <Button title="Go to Signin" onPress={() => navigation.navigate('Signin')} />
-      <Button title="Go to mainFlow" onPress={() => navigation.navigate('mainFlow')} />
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Spacer>
+        <Input label="Email" />
+      </Spacer>
+      <Spacer>
+        <Input label="Password" />
+      </Spacer>
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+
+      {/* <Button title="Go to Signin" onPress={() => navigation.navigate('Signin')} /> */}
+      {/* <Button title="Go to mainFlow" onPress={() => navigation.navigate('mainFlow')} /> */}
     </View>
   );
 };
 
-const styles = StyleSheet.create({
+SignupScreen.navigationOptions = {
+  header: null,
+}
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 200,
+  }
 });
 
 export default SignupScreen;
