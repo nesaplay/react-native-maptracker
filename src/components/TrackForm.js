fbeecd580143488469f5b52ece4d21587550860f
Nenad Radovanovic <nash.radovanovich@gmail.com>
2019-10-07 16:31:07 +0200

    tracking locations for recording

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,14 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackForm = ({}) => {
+  const {
+    state: { name, recording, locations },
+    startRecording,
+    stopRecording,
+    changeName,
+  } = useContext(LocationContext);
+  console.log(locations.length);
+
   return (
     <>
       <Spacer>
-        <Input placeholder="Recording #1..." />
+        <Input placeholder='Recording #1...' onChangeText={changeName} value={name} />
       </Spacer>
-      <Button title='Start Recording' />
+      {recording ? (
+        <Button title='Stop' onPress={stopRecording} />
+      ) : (
+        <Button title='Start Recording' onPress={startRecording} />
+      )}
     </>
   );
 };
